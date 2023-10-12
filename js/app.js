b1dc4727c790f07e3324f44d1c972f9a007bac44
Augusta Ellison <gustaellison@gmail.com>
2023-10-12 12:21:31 -0400

    formatting changes

--- js/app.js
+++ js/app.js
@@ -17,8 +17,6 @@ const ENEMIES = [
     'https://pyxis.nymag.com/v1/imgs/0db/66f/467b1483e421ea9a11d0847cfa51a77710-hottest-orc-2.rsquare.w350.jpg',
     'https://www.boredpanda.com/blog/wp-content/uploads/2023/04/clipimage-644bb65da66e2__700.jpg',
     'https://www.boredpanda.com/blog/wp-content/uploads/2023/04/clipimage-644bb85f72a09__700.jpg',
-    'https://www.boredpanda.com/blog/wp-content/uploads/2023/04/clipimage-644bb8fa11f4e__700.jpg',
-    'https://www.boredpanda.com/blog/wp-content/uploads/2023/04/clipimage-644bb951643cf__700.jpg',
     'https://images-wixmp-ed30a86b8c4ca887773594c2.wixmp.com/f/1dbc1935-6542-4ee3-822f-135cff4ba62c/df98de6-3bd0eb87-f58b-4726-ad76-ce20a4c8f570.png/v1/fill/w_485,h_605/gollum__1____transparent__by_speedcam_df98de6-fullview.png?token=eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzI1NiJ9.eyJzdWIiOiJ1cm46YXBwOjdlMGQxODg5ODIyNjQzNzNhNWYwZDQxNWVhMGQyNmUwIiwiaXNzIjoidXJuOmFwcDo3ZTBkMTg4OTgyMjY0MzczYTVmMGQ0MTVlYTBkMjZlMCIsIm9iaiI6W1t7ImhlaWdodCI6Ijw9NjA1IiwicGF0aCI6IlwvZlwvMWRiYzE5MzUtNjU0Mi00ZWUzLTgyMmYtMTM1Y2ZmNGJhNjJjXC9kZjk4ZGU2LTNiZDBlYjg3LWY1OGItNDcyNi1hZDc2LWNlMjBhNGM4ZjU3MC5wbmciLCJ3aWR0aCI6Ijw9NDg1In1dXSwiYXVkIjpbInVybjpzZXJ2aWNlOmltYWdlLm9wZXJhdGlvbnMiXX0.Q25iqft9m-DIaaiS-3gSvVU69efaZnVxfohOxSt5wVM']
 
 const fighterEls = document.querySelectorAll('ul.fighters > div')
@@ -27,8 +25,8 @@ const fighterPowerEls = document.querySelectorAll('ul > div > p')
 const championText = document.getElementById('champion-text')
 
 let fighterArenaImgEl = document.getElementById('your-fighter')
-const enemyArenaImgEl = document.getElementById('enemy-fighter')
 const fighterArenaPowerEl = document.getElementById('your-power')
+const enemyArenaImgEl = document.getElementById('enemy-fighter')
 let enemyArenaPowerEl = document.getElementById('enemy-power')
 
 const powerupEls = document.querySelectorAll('div.powerups > p')
@@ -57,7 +55,7 @@ playAgainEl.addEventListener('click', init)
 fightEl.addEventListener('click', clickFight)
 
 
-fighterEls.forEach(item => {
+fighterImageEls.forEach(item => {
     item.addEventListener('click', chooseCards)
 })
 
@@ -80,6 +78,9 @@ function chooseCards(evt) {
     console.log(evt.target)
     currentFighterPower = fighterArenaPowerEl.innerText
     clicks = clicks + 1
+    enemyArenaImgEl.src = 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/46/Question_mark_%28black%29.svg/800px-Question_mark_%28black%29.svg.png'
+    enemyArenaPowerEl.textContent = '?'
+    winnerEl.textContent = ''
 
 
 }
@@ -97,11 +98,11 @@ function clickFight(evt) {
 
 function findWinner() {
     if (currentFighterPower < enemyPower) {
-        return 'the enemy beat you'
+        return 'The enemy beat you!'
     } else if (currentFighterPower > enemyPower) {
-        return 'you beat the enemy'
+        return 'You beat the enemy!'
     } else {
-        return 'it was a tie'
+        return 'It was a tie.'
     }
 }
 
@@ -125,7 +126,7 @@ function getChampion() {
         return championText.innerText = 'You Won the Battle!'
     } else if (playerScore < enemyScore && clicks === 6) {
         return championText.innerText = 'The Enemy Won the Battle!'
-    } else if (clicks === 6) {
+    } else if (playerScore === enemyScore && clicks === 6) {
         return championText.innerText = 'No One Won the Battle'
     }
 }
@@ -140,7 +141,6 @@ function init() {
         { points: Math.floor(Math.random() * 10 + 1), imgUrl: 'https://media-cldnry.s-nbcnews.com/image/upload/t_fit-1500w,f_auto,q_auto:best/streams/2013/June/130606/6C7757975-130606-ent-cersei-hmed.jpg' },
         { points: Math.floor(Math.random() * 10 + 1), imgUrl: 'https://thewitcher3.wiki.fextralife.com/file/The-Witcher-3/ciri-icon-the-witcher-3-wild-hunt-wiki-guide-min.png' },
         { points: Math.floor(Math.random() * 10 + 1), imgUrl: 'https://upload.wikimedia.org/wikipedia/en/thumb/e/e9/Gandalf600ppx.jpg/170px-Gandalf600ppx.jpg' },
-        { points: Math.floor(Math.random() * 10 + 1), imgUrl: 'https://www.iment.com/maida/tv/lordoftherings/galadriel-small.jpg' },
         { points: Math.floor(Math.random() * 10 + 1), imgUrl: 'https://upload.wikimedia.org/wikipedia/en/0/03/Walter_White_S5B.png' },
         { points: Math.floor(Math.random() * 10 + 1), imgUrl: 'https://upload.wikimedia.org/wikipedia/en/5/50/Tyrion_Lannister-Peter_Dinklage.jpg' },
         { points: Math.floor(Math.random() * 10 + 1), imgUrl: 'https://e00-marca.uecdn.es/assets/multimedia/imagenes/2022/01/21/16427582365856.jpg' },
